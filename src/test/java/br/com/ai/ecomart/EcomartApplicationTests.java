b66aefb18e200ce04d731ed09e7650320accff50
Rafael540 <alves540@hotmail.com>
2026-02-11 14:06:44 -0300

    Primeiro commit

---
+++ src/test/java/br/com/ai/ecomart/EcomartApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ai.ecomart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
